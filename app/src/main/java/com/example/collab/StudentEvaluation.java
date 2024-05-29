e63985053ae6d5a74cf3a6aaae0e1818795c4d3a
İpek <zekiyeipek_ozmen@hotmail.com>
2024-05-29 21:59:16 +0300

    evaluation update

--- app/src/main/java/com/example/collab/StudentEvaluation.java
+++ app/src/main/java/com/example/collab/StudentEvaluation.java
@@ -1,23 +1,19 @@
 package com.example.collab;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MyTracking#newInstance} factory method to
+ * Use the {@link StudentEvaluation#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MyTracking extends Fragment {
+public class StudentEvaluation extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +24,7 @@ public class MyTracking extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MyTracking() {
+    public StudentEvaluation() {
         // Required empty public constructor
     }
 
@@ -38,11 +34,11 @@ public class MyTracking extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MyTrackingFragment.
+     * @return A new instance of fragment StudentEvaluation.
      */
     // TODO: Rename and change types and number of parameters
-    public static MyTracking newInstance(String param1, String param2) {
-        MyTracking fragment = new MyTracking();
+    public static StudentEvaluation newInstance(String param1, String param2) {
+        StudentEvaluation fragment = new StudentEvaluation();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,21 +59,6 @@ public class MyTracking extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.my_tracking, container, false);
-
-        // Find the RadioButton by its ID
-        RadioButton evaluationPhaseButton = view.findViewById(R.id.evaluationPhaseButton);
-
-        // Set an OnClickListener to the evaluationPhaseButton
-        evaluationPhaseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Use NavController to navigate to the StudentEvaluation fragment
-                NavController navController = Navigation.findNavController(view);
-                navController.navigate(R.id.studentEvaluation);
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.student_evaluation, container, false);
     }
 }
